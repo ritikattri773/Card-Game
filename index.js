c28506c483b7433b967688861c874bdfb82b0435
ritikattri773 <ritikattri773@gmail.com>
2025-02-10 04:00:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const cardObjectDefintions = [
+    {id:1,imagePath:"images/card-KingHearts.png"},
+    {id:2,imagePath:"images/card-JackClubs.png"},
+    {id:3,imagePath:"images/card-QueenDiamonds.png"},
+    {id:4,imagePath:"images/card-AceSpades.png"}
+]
+
+
+function createCard(cardItems){
+    
+}
