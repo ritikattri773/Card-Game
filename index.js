7198ebb3bc30518eee558ff93fde7ec949258f3b
ritikattri773 <ritikattri773@gmail.com>
2025-02-10 13:37:57 +0530

    LAST SOME CHAGES

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cardObjectDefinitions = [
     {id:1, imagePath:'/images/card-KingHearts.png'},
-    {id:2, imagePath:'/images/card-JackClubs.png'},
+    {id:2, imagePath:'images/card-JackClubs.png'},
     {id:3, imagePath:'/images/card-QueenDiamonds.png'},
     {id:4, imagePath:'/images/card-AceSpades.png'}
 ]
